715c42b4ac22effdb74148be0e20d4156a6c4867
Shibin-Ez <ezshibin@gmail.com>
2024-06-19 12:17:47 +0530

    update student path

--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createStudentsFromCSV, getStudentById, getStudents } from '../controllers/students.js';
+import { createStudentsFromCSV, getStudentById, getStudents, updateStudentWithChoices } from '../controllers/students.js';
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post("/csv", createStudentsFromCSV);
 // READ
 router.get("/", getStudents);
 router.get("/student/:id", getStudentById)
+
+// UPDATE
+router.patch("/student/:id/choices", updateStudentWithChoices);
 
 export default router;
